bad8b43d281aa2f4c5dae8d1cdc73440c1a2f532
Max Linke <max_linke@gmx.de>
2017-11-17 21:39:08 +0100

    clean up org config

--- layers/org-max/config.el
+++ layers/org-max/config.el
@@ -2,15 +2,11 @@
   (setq org-agenda-files (list "~/org/organizer.org"))
 
   (setq org-todo-keywords
-        '((sequence "TODO(t)" "WAITING(w)" "Active(a)" "|" "DONE(d!)" "CANCELED(c@)")))
+        '((sequence "TODO(t)" "WAITING FOR(w)" "SOMEDAY(s)" "|" "DONE(d!)" "CANCELED(c@)")))
 
   (setq org-capture-templates
-        '(("p" "PRIVAT-TODO" entry (file+headline "/home/max/org/organizer.org" "Private")
-           "* TODO %? %^g\n %u")
-          ("w" "WORK-TODO" entry (file+headline "/home/max/org/organizer.org" "Work")
-           "* TODO %? %^g\n %u")
-          ("o" "FOSS-TODO" entry (file+headline "/home/max/org/organizer.org" "Foss")
-           "* TODO %? %^g\n %u")))
+        '(("p" "capture" entry (file+headline "/home/max/org/organizer.org" "Inbox")
+           "* %? \n")))
 
   ;; setup org publish
   (setq org-export-async-debug t)
@@ -29,18 +25,6 @@
     (setq org-export-backends (quote (ascii beamer html icalendar latex md)))
     (add-to-list 'org-latex-packages-alist '("" "listings"))
     (add-to-list 'org-latex-packages-alist '("" "color"))
-    (add-to-list 'org-latex-classes
-                 '("complexes-style"
-                   "\\documentclass{book}
-                 [DEFAULT-PACKAGES]
-                 [PACKAGES]
-                 [EXTRA]"
-                   ("\\chapter{%s}" . "\\chapter{%s}")
-                   ("\\section{%s}" . "\\section{%s}")
-                   ("\\subsection{%s}" . "\\subsection{%s}")
-                   ("\\subsubsection{%s}" . "\\subsubsection{%s}")
-                   ("\\paragraph{%s}" . "\\paragraph{%s}")
-                   ("\\subparagraph{%s}" . "\\subparagraph{%s}")))
     (add-to-list 'org-latex-classes '("revtex4-1"
                                       "\\documentclass{revtex4-1}
                                         [NO-DEFAULT-PACKAGES]
